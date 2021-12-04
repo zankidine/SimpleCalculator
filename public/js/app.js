83e7ffd32e7ef29edb1dcd1f47c0bfdc736013f2
Zankidine ABDOU <zankidine.abdou60@gmail.com>
2021-12-04 18:45:14 +0100

    Description of JavaScript logic

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+/**
+ * Simple Calculator JS
+ */
+
+// External variables
+// Number button
+const zero = document.querySelector('.zero');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const tree = document.querySelector('.tree');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const sevent = document.querySelector('.sevent');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+
+// Operator
